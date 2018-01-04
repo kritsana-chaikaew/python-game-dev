d5b5b10f620662315c3c7514ae88859fdff95634
Kritsana <kritsana.chai@ku.th>
2018-01-05 01:34:25 +0700

    Add brick class

--- pong/chapter1_01.py
+++ pong/chapter1_01.py
@@ -59,6 +59,28 @@ class Paddle(GameObject):
             if self.ball is not None:
                 self.ball.move(offset, 0)
 
+class Brick(GameObject):
+    COLORS = {1: '#999999', 2: '#555555', 3: '#222222'}
+
+    def __init__(self, canvas, x, y, hits):
+        self.width = 75
+        self.height = 20
+        self.hits = hits
+        color = Brick.COLORS[hits]
+        item = canvas.create_rectangle(
+                x-(self.width/2), y-(self.height/2),
+                x+(self.width/2), y+(self.height/2),
+                fill=color, tags='brick')
+        super(Brick, self).__init__(canvas, item)
+
+    def hit(self):
+        self.hits -= 1
+        if self.hits == 0:
+            self.delete()
+        else:
+            self.canvas.itemconfig(self.item,
+                    fill=Brick.COLORS[self.hits])
+
 if __name__ == '__main__':
     root = tk.Tk()
     root.title('Hello, Pong!')
