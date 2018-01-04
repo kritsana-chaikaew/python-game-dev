7af029fed9e7d011a52847a8c3c0ba299888cf40
Kritsana <kritsana.chai@ku.th>
2018-01-05 01:23:33 +0700

    Add ball and paddle

--- pong/chapter1_01.py
+++ pong/chapter1_01.py
@@ -7,8 +7,8 @@ class Game(tk.Frame):
         self.width = 600
         self.height = 400
         self.canvas = tk.Canvas(self, bg='#aaaaff',
-            width=self.width,
-            height=self.height)
+                width=self.width,
+                height=self.height)
         self.canvas.pack()
         self.pack()
 
@@ -25,6 +25,39 @@ class GameObject(object):
 
     def delete(self):
         self.canvas.delete(self.item)
+
+class Ball(GameObject):
+    def __init__(self, canvas, x, y):
+        self.radius = 10
+        self.direction = [1, -1]
+        self.speed = 10
+        item = canvas.create_oval(
+                x-self.radius, y-self.radius,
+                x+self.radius, y+self.radius,
+                fill='white')
+        super(Ball, self).__init__(canvas, item)
+
+class Paddle(GameObject):
+    def __init__(self, canvas, x, y):
+        self.width = 80
+        self.height = 10
+        self.ball = None
+        item = canvas.create_rectangle(
+                x-(self.width/2), y-(self.height/2),
+                x+(self.width/2), y+(self.height/2),
+                fill='blue')
+        super(Paddle, self).__init__(canvas, item)
+
+    def set_ball(self, ball):
+        self.ball = ball
+
+    def move(self, offset):
+        coords = self.get_position()
+        width = self.canvas.winfo_width()
+        if coords[0] + offset >= o and coords[2] + offset <= width:
+            super(Paddle, self).move(offset, 0)
+            if self.ball is not None:
+                self.ball.move(offset, 0)
 
 if __name__ == '__main__':
     root = tk.Tk()
