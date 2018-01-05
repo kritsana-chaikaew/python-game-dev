b01f5783c40ad358e70ba95332f92d2b9da58002
Kritsana <kritsana.chai@ku.th>
2018-01-05 18:00:15 +0700

    Add actors

--- invader/cocos2d.py
+++ invader/cocos2d.py
@@ -1,17 +1,29 @@
 import cocos
+import cocos.collision_model as cm
+import cocos.euclid as eu
 
 from collections import defaultdict
 from pyglet.window import key
+
+class Actor(cocos.sprite.Sprite):
+    def __init__(self, x, y, color):
+        super(Actor, self).__init__('ball.png', color=color)
+        self.position = pos = eu.Vector2(x, y)
+        self.cshape = cm.CircleShape(pos, self.width/2)
 
 class MainLayer(cocos.layer.Layer):
     is_event_handler = True
     def __init__(self):
         super(MainLayer, self).__init__()
-        self.player = cocos.sprite.Sprite('ball.png')
-        self.player.color = (0, 0, 255)
-        self.player.position = (320, 240)
+        self.player = Actor(320, 240, (0, 0, 255))
         self.add(self.player)
+        for pos in [(100, 100), (540, 380),\
+                (540, 100), (100, 380)]:
+            self.add(Actor(pos[0], pos[1], (255, 0, 0)))
 
+        cell = self.player.width * 1.25
+        self.collman = cm.CollisionManagerGrid(0, 640,
+                0, 480, cell, cell)
         self.speed = 100.0
         self.pressed = defaultdict(int)
         self.schedule(self.update)
