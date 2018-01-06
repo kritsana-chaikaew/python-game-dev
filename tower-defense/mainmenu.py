e467f2da36e14ddf5bad0b13fea01732e5f370cd
Kritsana <kritsana.chai@ku.th>
2018-01-06 22:01:24 +0700

    Complete main menu

--- tower-defense/mainmenu.py
+++ tower-defense/mainmenu.py
@@ -1,8 +1,13 @@
-import cocos
-from cocos.menu import *
-from cocos.director import director
-import pyglet.app
+import cocos.menu
+import cocos.scene
+import cocos.layer
 import cocos.actions as ac
+from cocos.director import director
+from cocos.scene.transitions import FadeTRTransition
+
+import pyglet.app
+
+from gamelayer import new_game
 
 class MainMenu(cocos.menu.Menu):
     def __init__(self):
@@ -22,3 +27,16 @@ class MainMenu(cocos.menu.Menu):
         items.append(MenuItem('Quit', pyglet.app.exit))
         self.create_menu(item, ac.ScaleTo(1.25, duration=0.25),
                 ac.ScaleTo(1.0, duration=0.25))
+
+    def on_new_game(self):
+        director.push(FadeTRTransition(new_game(), duration=2))
+
+    def show_fps(self, val):
+        director.show_FPS = val
+
+def new_menu():
+    scene = cocos.scene.Scene()
+    color_layer = cocos.layer.ColorLayer(205, 133, 63, 255)
+    scene.add(MainMenu(), z=1)
+    scene.add(color_layer, z=0)
+    return scene
