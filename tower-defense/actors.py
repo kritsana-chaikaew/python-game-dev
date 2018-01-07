d8317fafa940bb10e94955de93432fcc9c5b00b6
Kritsana <kritsana.chai@ku.th>
2018-01-07 11:57:46 +0700

    Add turret class

--- tower-defense/actors.py
+++ tower-defense/actors.py
@@ -10,4 +10,14 @@ class Actor(cocos.sprite.Sprite):
     @property
     def cshape(self):
         self._cshape.center = eu.Vector2(self.x, self.y)
-        return self._cshape    
+        return self._cshape
+
+class Turret(Actor):
+    def __init__(self, x, y):
+        super(Turret, self).__init__('turret.png', x, y)
+        self.add(cocos.sprite.Sprite('rang.png', opacity=50, scale=5))
+        self.cshape.r = 125.0
+        self.target = None
+        self.period = 2.0
+        self.reload = 0.0
+        self.schedule(self._shoot)
